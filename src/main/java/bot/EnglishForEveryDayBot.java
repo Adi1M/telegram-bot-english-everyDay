dafb024e97250206ddaa815a8cc73777ead1d3e4
Syrhundred <91780657+Syrhundred@users.noreply.github.com>
2023-01-10 00:07:44 +0600

    We did start with connection to database ^^

--- src/main/java/bot/EnglishForEveryDayBot.java
+++ src/main/java/bot/EnglishForEveryDayBot.java
@@ -4,10 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @Slf4j
 public class EnglishForEveryDayBot extends TelegramLongPollingBot {
     private final String botUsername;
     private final String botToken;
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/engBot";
+    static final String USER = "postgres";
+    static final String PASS = "postgrespass";
 
     public EnglishForEveryDayBot(String botUsername, String botToken){
         this.botToken = botToken;
@@ -26,6 +34,18 @@ public class EnglishForEveryDayBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.getMessage().getText().equals("/start")) {
+            try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                Statement stmt = conn.createStatement();
+            ) {
+                System.out.println("Inserting records into the table...");
+                long chatId = update.getMessage().getChatId();
 
+                String sql = "INSERT INTO users VALUES (" + chatId + ", 1, true)";
+                stmt.executeUpdate(sql);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
